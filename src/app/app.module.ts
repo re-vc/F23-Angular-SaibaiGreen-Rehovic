4a142fd46f778b76c6d1c60e05c2905d9baa82fb
re-vc <frederikrehovic@gmail.com>
2023-02-22 21:31:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FtrComponent } from './ftr/ftr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FtrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ftr/ftr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ftr',
+  templateUrl: './ftr.component.html',
+  styleUrls: ['./ftr.component.scss']
+})
+export class FtrComponent {
+
+}
